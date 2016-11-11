f0ecdc061ffc16f48813752750a2de2644bf042f
Tomas Fagerbekk <tomas@webutvikling.org>
2016-11-11 22:57:47 +0100

    Demonstrate React router

---
+++ src/containers/NoMatch.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class NoMatch extends Component {
+  render() {
+    return (
+      <div>
+        These aren't the droids you're looking for
+      </div>
+    );
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,5 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './containers/App';
+import Test from './containers/Test';
+import NoMatch from './containers/NoMatch';
+import { render } from 'react-dom'
+import { Router, Route, Link, browserHistory } from 'react-router'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+render((
+  <Router history={browserHistory}>
+    <Route path="/" component={App} />
+    <Route path="test" component={Test}/>
+  	<Route path="*" component={NoMatch}/>
+  </Router>
+), document.getElementById('root'))
